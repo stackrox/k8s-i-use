50ada194ce2f678fb7bc3876df063917b2a8fac9
Matthias <matthias@stackrox.com>
2021-02-11 17:18:54 +0100

    Introduce color coded tables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,7 +37,8 @@ function Home() {
     });
     return (
         <>
-            <h2>Click on the object of your choice to get started.</h2>
+            <h1>k8s-iuse</h1>
+            <p>Click on the object of your choice to get started.</p>
             {gvkLinks}
         </>
     );
@@ -55,7 +56,7 @@ function SingleObj() {
         )
     }
     const fields = singleObj.fields.map((item) => 
-        <li>{item.name}
+        <li key={item.name}>{item.name}
             <ul>
                 {item.seen_in.length > 0 &&
                 (<li><i style={{color: "#239B56"}}>Available</i> in versions: {prettyPrintVersions(item.seen_in)}</li>)}
@@ -63,19 +64,59 @@ function SingleObj() {
                 (<li><i style={{color: "#B03A2E"}}>Deprecated</i> but available in versions: {prettyPrintVersions(item.deprecated_in)}</li>)}
             </ul>
         </li>);
+
+    const fieldRows = singleObj.fields.map((item) => constructructRow(item));
+    const kindRow = constructructRow(singleObj);
+
     return (
         <>
             <Link to={"/"}>Back to home page</Link>
             <h2> {prettyPrintObj(singleObj)} </h2>
+            
+            <table class="styled-table">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>1.7</th><th>1.8</th><th>1.9</th><th>1.10</th><th>1.11</th><th>1.12</th>
+                        <th>1.13</th><th>1.14</th><th>1.15</th><th>1.16</th><th>1.17</th><th>1.18</th>
+                        <th>1.19</th><th>1.20</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {kindRow}
+                </tbody>
+            </table>
             {singleObj.seen_in.length > 0 &&
                 (<p> <i style={{color: "#239B56"}}>Stable</i> and available in Kube versions {prettyPrintVersions(singleObj.seen_in)}</p>)}
+            <table class="styled-table">
+                <caption>Fields</caption>
+                <thead>
+                    <tr>
+                    <th>Field Name</th>
+                        <th>1.7</th><th>1.8</th><th>1.9</th><th>1.10</th><th>1.11</th><th>1.12</th>
+                        <th>1.13</th><th>1.14</th><th>1.15</th><th>1.16</th><th>1.17</th><th>1.18</th>
+                        <th>1.19</th><th>1.20</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                    <th>Field Name</th>
+                        <th>1.7</th><th>1.8</th><th>1.9</th><th>1.10</th><th>1.11</th><th>1.12</th>
+                        <th>1.13</th><th>1.14</th><th>1.15</th><th>1.16</th><th>1.17</th><th>1.18</th>
+                        <th>1.19</th><th>1.20</th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    {fieldRows}
+                </tbody>
+            </table>
+            
              {singleObj.deprecated_in.length > 0 &&
                 (<p> <i style={{color: "#B03A2E"}}>Deprecated</i> but available in Kube versions {prettyPrintVersions(singleObj.deprecated_in)}</p>)}
             {singleObj.fields.length > 0 &&
                 (<h3>Fields: </h3>)}
             {singleObj.fields.length > 0 &&
                 (<ul>{fields}</ul>)}
-                
         </>
     );
 }
@@ -91,9 +132,70 @@ function prettyPrintVersions(versions) {
     return `v1.${versions[0]} - v1.${versions.slice(-1)[0]}`;
 }
 
+function constructructRow(entry) {
+    var versions = [7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    return <tr>
+        <td>{entry.name}</td>
+        {versions.map((version) => 
+            <td className={entry.seen_in.includes(version) ? "cl-yes":"cl-no"}></td> )}
+    </tr>
+}
+
 function constructLink(obj) {
     const elems = [obj.group, obj.version, obj.kind];
     return elems.filter(elem => elem).map(elem => elem.toLowerCase()).join('-');
 }
 
+
+var clickEvent = "ontouchstart" in window ? "touchend" : "click",
+classMethods = ["remove", "add"],
+stringArray = ["Add more contrast", "Remove additional contrast", "Dark Mode", "Light Mode"];
+
+function createControls() {
+    var contrastButton = document.createElement('button');
+      contrastButton.id = "contrast";
+      contrastButton.classList.add('cont-inv');
+      contrastButton.innerText = stringArray[0];
+      contrastButton.tabIndex = 1;
+  
+    var nightModeButton = document.createElement('button');
+      nightModeButton.id = "invmode";
+      nightModeButton.classList.add('cont-inv');
+      nightModeButton.innerText = stringArray[2];
+      nightModeButton.tabIndex = 2;
+    document.body.appendChild(contrastButton);
+    document.body.appendChild(nightModeButton);
+  }
+
+  function someControl(id, textArr, className) {
+    var el = document.getElementsByTagName("html")[0];
+    var acbox = document.getElementById(id),
+      textNode = acbox.firstChild,
+      toggled = false;
+    acbox.addEventListener(
+      clickEvent,
+      function() {
+        var selector = Number((toggled = !toggled));
+        textNode.data = textArr[selector];
+        el.classList[classMethods[selector]](className);
+      },
+      false
+    );
+  }
+  
+  function addContrastControl() {
+    someControl(
+      "contrast",
+      [stringArray[0], stringArray[1]],
+      "contrast"
+    );
+  }
+  
+  function addInvertedControl() {
+    someControl("invmode", [stringArray[2], stringArray[3]], "inverted");
+  }
+  
+  createControls();
+  addContrastControl();
+  addInvertedControl();
 export default App;
